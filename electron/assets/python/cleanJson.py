7d69f70d3fad8b794f291f6c31522fbc2e5be5b1
Olivia Croteau <oliviacroteau667@gmail.com>
2025-02-27 12:49:44 -0500

    modified message ids in cleanJson

--- electron/assets/python/cleanJson.py
+++ electron/assets/python/cleanJson.py
@@ -8,21 +8,16 @@ def filterMessages(jsonData):
             message["text"] = None
         if message.get('reactions'):
             del message['reactions']
+    return jsonData
 
-# Step 2: Modify reply IDs
-def modifyReplies(jsonData):
-    incrementing_value = 100
+def modifyIds(jsonData):
+    resultId = str(jsonData["id"])
+    resultId = resultId + "-"
     for message in jsonData["messages"]:
-        if "reply_to_message_id" in message:
-            # Update 'id' to the value of 'reply_to_message_id'
-            message["id"] = message["reply_to_message_id"]
-
-            # Rename 'reply_to_message_id' to 'reply_id' with the new incremented value
-            message["reply_id"] = incrementing_value
-            incrementing_value += 1
-
-            # Remove the old 'reply_to_message_id' field
-            del message["reply_to_message_id"]
+        messageId = str(message['id'])
+        newId = resultId + messageId
+        message['id'] = newId
+    return jsonData
 
 # Step 3: Process text entities
 def processTextEntities(jsonData):
@@ -37,16 +32,15 @@ def processTextEntities(jsonData):
         else:
             message.pop('text', None)  # Safely remove 'text' if it doesn't exist
         message.pop('text_entities', None)  # Safely remove 'text_entities'
+    return jsonData
 
 def cleanJson(resultJson):
     with open(resultJson, 'r', encoding='utf-8') as f:
         jsonData = json.load(f)
 
-        # Call each function sequentially
-        print(f"Cleaning file {resultJson}")
-        filterMessages(jsonData)
-        modifyReplies(jsonData)
-        processTextEntities(jsonData)
+        jsonData = filterMessages(jsonData)
+        jsonData = processTextEntities(jsonData)
+        jsonData = modifyIds(jsonData)
 
     with open(resultJson, 'w', encoding='utf-8') as f:
         json.dump(jsonData, f, ensure_ascii=False, indent=4)
